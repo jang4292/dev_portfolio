be8c3a2bcbd35618a251079ea1a92e89c4d080b0
JangYunHwan <janghyunki17@naver.com>
2025-09-11 15:23:20 +0900

    Init rn project for studying about handson (#28)

---
+++ playground/rn_expo_calculator/src/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Button from './components/Button';
+
+export default function App() {
+  console.log("expo React Native")
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text stype={styles.text}>Calc App</Text>
+      <Button
+        title="Test"
+        onPress={() => console.log('click!')}
+        color="purple" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: '700',
+    color: 'green'
+  }
+});
