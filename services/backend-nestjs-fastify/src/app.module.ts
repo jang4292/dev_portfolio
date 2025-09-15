e2742386d4daaa54b212a7c561736524e34d5188
JangYunHwan <janghyunki17@naver.com>
2025-09-15 23:54:36 +0900

    Develop (#37)
    * Update gitignore
    
    * Update static site for black jack project with cc (#30)
    
    * Init rn project for studying about handson (#28)
    
    * sync (#34)
    
    * Develop (#32)
    
    * Update gitignore
    
    * Update static site for black jack project with cc (#30)
    
    * Init rn project for studying about handson (#28)
    
    * Develop (#33)
    
    * Update gitignore
    
    * Update static site for black jack project with cc (#30)
    
    * Init rn project for studying about handson (#28)
    
    * sync (#35)
    
    * Develop (#32)
    
    * Update gitignore
    
    * Update static site for black jack project with cc (#30)
    
    * Init rn project for studying about handson (#28)
    
    * Develop (#33)
    
    * Update gitignore
    
    * Update static site for black jack project with cc (#30)
    
    * Init rn project for studying about handson (#28)
    
    * Update files for initializing about user data

--- services/backend-nestjs-fastify/src/app.module.ts
+++ services/backend-nestjs-fastify/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { UserController } from 'src/list/user.controller';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,7 +7,7 @@ import { ListController } from './list/list.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ListController],
+  controllers: [AppController, ListController, UserController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
