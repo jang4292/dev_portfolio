83fd7f80efaa1d64555fdb5e7b10d9c71a35fd4f
jangyunhwan <jangyunhwan@gmail.com>
2025-09-16 20:20:28 +0900

    Init Update python main file

--- services/python-fastapi/app/main.py
+++ services/python-fastapi/app/main.py
@@ -1,3 +1,5 @@
+import uvicorn
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +7,6 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", reload=True)
