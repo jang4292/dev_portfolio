948d18fc8d4973d61fbfe0e73d1e3318508b11d4
jang4292 <jangyunhwan@gmail.com>
2025-12-18 18:48:55 +0900

    Android > Splash Screen Custom View

--- labs/blackjackai_test/app/build.gradle.kts
+++ labs/blackjackai_test/app/build.gradle.kts
@@ -16,6 +16,8 @@ android {
     versionName = "1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+    buildConfigField("String", "BASE_URL", "\"http://192.168.0.11:7456/web-mobile/web-mobile/index.html\"") // 기본 URL
   }
 
   buildTypes {
@@ -52,8 +54,11 @@ dependencies {
 //  implementation("com.android.billingclient:billing-ktx:<latest>") // IAP
 
   implementation("androidx.core:core-ktx:1.13.1")
-  implementation("androidx.appcompat:appcompat:1.7.0")
+  implementation("androidx.appcompat:appcompat:1.7.1")
   implementation("com.google.android.material:material:1.12.0")
+
+  implementation("androidx.core:core-splashscreen:1.2.0")
+
 
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.lifecycle.runtime.ktx)
